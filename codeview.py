7f6ee26917a94eb34bbda1ea95556a96a394b947
Scott Mansell <phiren@gmail.com>
2024-03-26 18:21:36 +1300

    As I thought: Constants use variable length ints

--- codeview.py
+++ codeview.py
@@ -1,6 +1,25 @@
 from construct import *
 from constructutils import *
 
+# Some fields used this variable length integer encoding
+#   If the 16bit "typeOrVal" value is less than 0x8000, then the value is inlined (This might be limited to 8 bit values)
+#   Otherwise, it is treated as a type and a value of that type follows
+VarInt = FocusedSeq("value",
+            "typeOrVal" / Int16ul,
+            "value" / Switch(this.typeOrVal,
+                {
+                    0x8000: Int8sl,  # LF_CHAR
+                    0x8001: Int16sl, # LF_SHORT
+                    0x8002: Int16ul, # LF_USHORT
+                    0x8003: Int32sl, # LF_LONG
+                    0x8004: Int32ul, # LF_ULONG
+                    0x8009: Int64sl, # LF_QUADWORD
+                    0x800a: Int64ul, # LF_UQUADWORD
+                },
+                default=Computed(this.typeOrVal) # otherwise, value was small enough to be inlined
+            )
+        )
+
 CVSwitch = {}
 
 # decorator to assign classes to switch map
@@ -26,6 +45,14 @@ class CompileFlags(ConstructClass):
             Padding(4)
         ),
         "CompilerVersion" / PascalString(Int8ul, "ascii"),
+    )
+
+@CVRec(0x3) # S_CONSTANT_16t
+class Constant(ConstructClass):
+    subcon = Struct(
+        "Type" / Int16ul,
+        "Value" / VarInt, # cvinfo.h claims this is always a short, but it's actually VarInt
+        "Name" / PascalString(Int8ul, "ascii"),
     )
 
 @CVRec(0x4) # S_UDT_16t
--- tpi.py
+++ tpi.py
@@ -2,6 +2,8 @@
 from construct import *
 from constructutils import *
 
+from codeview import VarInt
+
 
 class StructProperty(ConstructClass):
     # bitfield describing class/struct/union/enum properties
@@ -72,25 +74,6 @@ class FieldAttributes(ConstructClass):
             "compgenx" / Flag, # doesn't exist, compiler generated function
         )
     )
-
-# Some fields used this variable length integer encoding
-#   If the 16bit "typeOrVal" value is less than 0x8000, then the value is inlined (This might be limited to 8 bit values)
-#   Otherwise, it is treated as a type and a value of that type follows
-VarInt = FocusedSeq("value",
-            "typeOrVal" / Int16ul,
-            "value" / Switch(this.typeOrVal,
-                {
-                    0x8000: Int8sl,  # LF_CHAR
-                    0x8001: Int16sl, # LF_SHORT
-                    0x8002: Int16ul, # LF_USHORT
-                    0x8003: Int32sl, # LF_LONG
-                    0x8004: Int32ul, # LF_ULONG
-                    0x8009: Int64sl, # LF_QUADWORD
-                    0x800a: Int64ul, # LF_UQUADWORD
-                },
-                default=Computed(this.typeOrVal) # otherwise, value was small enough to be inlined
-            )
-        )
 
 TpSwitch = {}
 
