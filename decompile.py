54fc9e5eb685b8d87e00060c6c975fb706952a8a
Scott Mansell <phiren@gmail.com>
2024-04-05 12:59:02 +1300

    Add a caching mechnisim for faster debug cycles

--- decompile.py
+++ decompile.py
@@ -2,8 +2,26 @@
 from program import Program
 from coff import Executable
 
-from itertools import pairwise, chain
-import x86
+import construct
+
+import pickle, os, sys, copyreg
+from collections import OrderedDict
+
+def better_getstate(self):
+    ret = OrderedDict(self)
+    try:
+        del ret["_io"]
+    except KeyError:
+        pass
+    try:
+        del ret["_stream"]
+    except KeyError:
+        pass
+
+    return ret
+
+# monkey patch container to filter out all io objects
+construct.Container.__getstate__ = better_getstate
 
 if __name__ == "__main__":
     import sys
@@ -11,9 +29,32 @@ if __name__ == "__main__":
     pdb_file = "../debug_build_beta/COPTER_D.PDB"
     exe_file = "../debug_build_beta/COPTER_D.EXE"
 
-    exe = Executable(exe_file)
+    try:
+        # check if we have a cache
+        modified = os.path.getmtime('cache.pkl')
+        with open("cache.pkl", "rb") as f:
+            cached_file, depends = pickle.load(f)
+            if cached_file != (pdb_file, exe_file):
+                raise Exception(f"wrong file")
 
-    p = Program(pdb_file, exe)
+            for file in depends:
+                if os.path.getmtime(file) > modified:
+                    raise Exception(f"{file} has been modified")
+
+            p = pickle.load(f)
+    except Exception as e:
+        print(f"Cache load failed because: {e}", file=sys.stderr)
+
+        exe = Executable(exe_file)
+        p = Program(pdb_file, exe)
+
+        # dump to cache
+        with open("cache.pkl", "wb") as f:
+            depends = [m.__file__ for m in sys.modules.values() if hasattr(m, '__file__') and m.__file__ not in (__file__, None)]
+            pickle.dump(((pdb_file, exe_file), depends), f)
+            pickle.dump(p, f)
+
+
     for lib in p.libraries.values():
         if lib.is_dll() or lib.name in ["OLDNAMES.lib", "LIBCMTD.lib"]:
             continue
